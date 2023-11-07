18e1865843e36e70d06ceca86f87090c149f10ff
Lesnyck <lesnyck2705@gmail.com>
2023-11-07 15:09:01 -0600

    First commit

---
+++ uva-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+
+        <Login></Login>
+
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
