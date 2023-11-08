e6f473995ceaa60f1b712af337db107d9cf4b2e2
Lesnyck <lesnyck2705@gmail.com>
2023-11-08 00:00:15 -0600

    second commit

--- uva-app/src/App.js
+++ uva-app/src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Components/Login'
+import Login from './Pages/Login/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignUp from './Pages/SignUp/SignUp';
+import uva from './assets/img/uva.png'
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login></Login>
+   
+    },
+
+    {
+      path: "/SignUp",
+      element: <SignUp></SignUp>
+    }
+
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header> <nav class="navbar bg-body-tertiary">
+  <div className="container">
+    <a className="navbar-brand" href="http://localhost:3000/">
+      <img src={uva} alt="uva logo" width="90" height="80"/>
+    </a>
+  </div>
+</nav>
+</header>
+      <img className='uvaLogo' src={uva} alt='uva logo'></img>
+      <p className='eslogan'> UVA App, organiza y administra <br/>
+       tu empresa de la mejor manera. </p>
+     
+<RouterProvider router={router}>
+      </RouterProvider>
+
+     
       
-
-        <Login></Login>
-
-      </header>
-
+      
       
     </div>
   );
